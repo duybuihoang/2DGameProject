38a40bbc671a14ed3f5583d14685c3d7d1ca5383
duybuihoang <91789217+duybuihoang@users.noreply.github.com>
2024-10-12 10:41:37 +0700

    feat: add sword attack animation

--- Assets/Script/Player/PlayerFiniteStateMachine/PlayerStates/SubState/MoveState.cs
+++ Assets/Script/Player/PlayerFiniteStateMachine/PlayerStates/SubState/MoveState.cs
@@ -29,7 +29,7 @@ public class MoveState : BaseMovementState
         base.LogicUpdate();
 
 
-        Movement?.CheckIfShouldFlip(mousePos,inputX);
+        //Movement?.CheckIfShouldFlip(mousePos,inputX);
 
         Movement?.SetVelocity(input, playerData.movementVelocity);
 

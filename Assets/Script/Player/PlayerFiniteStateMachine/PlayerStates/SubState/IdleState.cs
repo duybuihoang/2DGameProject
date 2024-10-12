13802d9fed8885d585a79efe8e4c0d0e81b31b8d
duybuihoang <91789217+duybuihoang@users.noreply.github.com>
2024-10-12 10:28:18 +0700

    Initial commit

---
+++ Assets/Script/Player/PlayerFiniteStateMachine/PlayerStates/SubState/IdleState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : BaseMovementState
+{
+    public IdleState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        Movement?.SetVelocityZero();
+
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        Movement?.CheckIfShouldFlip(mousePos,inputX);
+
+        if (!isExistingState)
+        {
+            if(input != Vector2.zero)
+            {
+                stateMachine.ChangeState(player.moveState);
+            }
+        }    
+    }
+}
